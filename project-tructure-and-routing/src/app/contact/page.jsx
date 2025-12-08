60b2e9129d2ff77e6c0403ed39dee3bb925524a7
Ujjal B. Allen <ujjal.vision02@gmail.com>
2025-12-09 03:02:37 +0600

    folder and files conventions

---
+++ project-tructure-and-routing/src/app/contact/page.jsx
@@ -0,0 +1,6 @@
+export default function Contact(){
+
+    return(
+        <div>This is a Contact Page!</div>
+    )
+}
---
+++ project-tructure-and-routing/src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-red-600 text-5xl">Hello world</h1>
+    </div>
+  );
+}
