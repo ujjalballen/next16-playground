5a1c32c17c48f0a1eb2c2af57ec3c705ee2bf049
Ujjal B. Allen <ujjal.vision02@gmail.com>
2025-12-28 01:58:35 +0600

    static open graph

--- middleware-metadata-and-seo/src/app/about/page.jsx
+++ middleware-metadata-and-seo/src/app/about/page.jsx
@@ -1,9 +1,26 @@
-  export const metadata = {
-    title: "About Page - SEO and Metadata",
-    description: "This is a About page",
-  };
+export const metadata = {
+  title: "About Page - SEO and Metadata",
+  description: "This is a About page",
+
+  // for specific page
+  openGraph: {
+    title: "About - Next.js Course",
+    description: "Learning about OG Img",
+    url: "http://localhost:3000/about",
+    siteName: "Next.js Learning",
+    images: [
+      {
+        url: "/latte-art.png",
+        width: 800,
+        height: 600,
+        alt: "latte-art",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 export default async function AboutPage() {
-
   return <div>This is a About PAGE!!!!!</div>;
 }
