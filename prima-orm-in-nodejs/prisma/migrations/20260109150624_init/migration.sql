2047b51aada4ffa6273317a1b3ad9f8b9ebc4701
Ujjal B. Allen <ujjal.vision02@gmail.com>
2026-01-10 02:58:17 +0600

    cover prisma queries like get, post, pagination, etc

---
+++ prima-orm-in-nodejs/prisma/migrations/20260109150624_init/migration.sql
@@ -0,0 +1,38 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `Post` table. All the data in the column will be lost.
+  - You are about to alter the column `title` on the `Post` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - Added the required column `authorId` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "description",
+ADD COLUMN     "authorId" INTEGER NOT NULL,
+ADD COLUMN     "content" TEXT,
+ADD COLUMN     "published" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ALTER COLUMN "title" SET DATA TYPE VARCHAR(255);
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "name" DROP NOT NULL,
+ALTER COLUMN "name" DROP DEFAULT;
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "bio" TEXT,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prima-orm-in-nodejs/server.js
+++ prima-orm-in-nodejs/server.js
@@ -1,20 +1,81 @@
 import express from "express"
-import { prisma } from "./lib/prisma";
+import { prisma } from "./lib/db.js";
 
 const app = express();
 const port = 3000
 
 app.use(express.json())
 
-app.get("/", async(req, res) => {
-    // await prisma.users.create({
-    //     data: {
-           
-    //         name: "Ujjal",
-
-    //     }
-    // })
+app.get("/", async (req, res) => {
     res.json({ message: "welcome" })
+})
+
+app.post("/api/users", async (req, res) => {
+    const { name, email } = req.body;
+
+    const user = await prisma.user.create({
+        data: {
+            name: name,
+            email: email,
+
+        },
+    })
+
+    console.log("user", user)
+
+    return res.json({ success: true })
+});
+
+// advanced at the same time
+app.post("/api/create-user", async (req, res) => {
+    const { name, email, bio } = req.body;
+
+    const user = await prisma.user.create({
+        data: {
+            name: name,
+            email: email,
+            profile: {
+                create: {
+                    bio: bio
+                }
+            }
+
+        },
+    })
+
+    return res.json(user)
+})
+
+
+app.post('/api/posts', async (req, res) => {
+    const { title, content } = req.body;
+
+    const post = await prisma.post.create({
+        data: {
+            title, content, authorId: 1
+        }
+    })
+
+    return res.json(post)
+})
+
+app.get("/api/posts", async (req, res) => {
+    const posts = await prisma.post.findMany()
+
+    return res.json(posts)
+})
+
+// pagination
+app.get("/api/get-posts", async (req, res) => {
+
+    const { skip, take } = req.query;
+
+    const posts = await prisma.post.findMany({
+        skip: Number(skip),
+        take: Number(take) // fix => how manny post we want to show in a page
+    });
+
+    return res.json(posts)
 })
 
 app.listen(port, () => {
